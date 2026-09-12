4eb154e35c3effba627825a66fad9453901d5c60
Divyansh Kumar <166832187+dkum34743@users.noreply.github.com>
2026-09-12 12:22:25 -0400

    Add emoji conversion functionality to faces.py

--- week0/faces.py
+++ week0/faces.py
@@ -1,1 +1,9 @@
+emoji=input()
 
+def convert(emoji):
+    if ":)" in emoji:
+        emoji=emoji.replace(":)", "🙂")
+    if ":(" in emoji:
+        emoji=emoji.replace(":(", "🙁")
+    print(emoji)
+convert(emoji)
